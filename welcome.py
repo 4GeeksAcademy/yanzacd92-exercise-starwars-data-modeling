192c4f1116421dbbeeb539e158f960aeb9778fde
yanzacd92 <121258474+yanzacd92@users.noreply.github.com>
2023-06-12 16:07:22 +0000

    added message

--- welcome.py
+++ welcome.py
@@ -3,6 +3,6 @@ print("""
 
 WELCOME GEEK! 🐍 + 💻 = 🤓
 
-- Change the src/models.py file and generate the UML Diagram by typing \033[94m$ pipenv run diagram\033[0m
+- Changes the src/models.py file and generate the UML Diagram by typing \033[94m$ pipenv run diagram\033[0m
 - Open the generated diagram by double clicking the \033[94m diagram.png \033[0m file.
 """)
